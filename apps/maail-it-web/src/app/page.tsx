801cad03d23848f351274a3b8e2250ca45516811
Aayushman Pratap <aayushmanpratap333@gmail.com>
2024-10-14 00:05:49 +0530

    added extra copy in landing page and add links in navbar

--- apps/maail-it-web/src/app/page.tsx
+++ apps/maail-it-web/src/app/page.tsx
@@ -5,10 +5,10 @@ export default function Home() {
   return (
     <div>
       <div className="absolute flex flex-col items-center top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-        <LogoSvg className="bg-black size-24 p-4 rounded-xl fill-primary-blue mb-6" />
-        <div className="flex flex-col items-center text-center">
+        <LogoSvg className="bg-black size-24 p-4 rounded-xl fill-primary-blue mb-8" />
+        <div className="flex flex-col items-center  text-center">
           <div className="flex items-center">
-            <h1 className="mr-3 text-5xl font-semibold">
+            <h1 className=" text-5xl font-semibold">
               Easy access to otp for testing
             </h1>
           </div>
@@ -17,10 +17,18 @@ export default function Home() {
             enter the slug of your required email and hit enter to start
             receiving emails on the mail, your id will be example@maailit.com
           </p>
-
-          <div className="w-full mt-6">
-            <EmailInput />
-          </div>
+          <p className="max-w-xl mt-1 text-base text-slate-600">
+            We dont care about
+            <span className="text-red-500 font-semibold">
+              {" "}
+              mobile users
+            </span>{" "}
+            get a bigger screen or dont use our app{" "}
+            <span className="text-green-500 font-bold">{`:)`}</span>
+          </p>
+        </div>
+        <div className="w-full mt-8">
+          <EmailInput />
         </div>
       </div>
     </div>
--- apps/maail-it-web/src/components/Navbar.tsx
+++ apps/maail-it-web/src/components/Navbar.tsx
@@ -2,11 +2,12 @@
 import React from "react";
 import LogoSvg from "./abstract";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 function Navbar() {
   const router = useRouter();
   return (
-    <div className="h-14 max-w-7xl mx-auto flex items-center justify-center bg-white mt-2 px-4 rounded-xl">
+    <div className="h-14 max-w-7xl mx-auto flex items-center justify-between bg-white mt-2 px-4 rounded-xl">
       <button
         type="button"
         onClick={() => router.replace("/")}
@@ -15,6 +16,24 @@ function Navbar() {
         <LogoSvg className="fill-primary-blue w-8" />
         <p className="font-bold text-xl">Maail It</p>
       </button>
+      <div className="flex items-center">
+        <p className="font-light text-slate-600">Made by:{` `} </p>
+        <div className="flex space-x-4">
+          <p>
+            <span className="mx-1 bg-blue-primary text-white px-2 py-1 rounded-md">
+              <Link target="_blank" href={` https://github.com/nivekithan`}>
+                Nivekithan
+              </Link>
+            </span>
+            {` `}
+            <span className="mx-1 bg-blue-primary text-white px-2 py-1 rounded-md">
+              <Link target="_blank" href={`https://github.com/Introfect`}>
+                Aayushman
+              </Link>
+            </span>
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
